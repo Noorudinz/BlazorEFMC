92b5b92d5f313dbf50264269f0d011b26e313c88
Noorudinz <noorudeens@yahoo.in>
2021-08-29 21:56:03 +0530

    Add or edit roles added

---
+++ BethanysPieShopHRM.Server/Components/AddEditRoles.razor.cs
@@ -0,0 +1,76 @@
+﻿using BethanysPieShopHRM.Server.Services;
+using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.Server.Components
+{
+    public partial class AddEditRoles : ComponentBase
+    {
+        public bool ShowDialog { get; set; }
+
+        public string StatusClass { get; set; } = string.Empty;
+        public string ErrorMsg { get; set; } = string.Empty;
+
+        public RolesVM role { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> CloseEventCallback { get; set; }
+
+        [Inject]
+        public IRolesDataService RoleDataService { get; set; }
+
+
+
+        public void Show()
+        {
+            ResetDialog();
+            ShowDialog = true;
+            StateHasChanged();
+        }
+
+        public async void ShowEdit(string Id)
+        {
+            //ResetDialog();
+            role = await RoleDataService.GetRoleDetails(Id);
+            ShowDialog = true;
+            StateHasChanged();
+
+        }
+        private void ResetDialog()
+        {
+            role = new RolesVM();
+        }
+
+        public void Close()
+        {
+            ShowDialog = false;
+            StateHasChanged();
+        }
+
+        protected async Task HandleValidSubmit()
+        {           
+            var response = await RoleDataService.AddRole(role);
+
+            if (response.IsSuccess)
+            {
+                ShowDialog = false;
+
+                await CloseEventCallback.InvokeAsync(true);
+                StateHasChanged();
+            }
+            else
+            {
+                StatusClass = "alert-danger";
+                ErrorMsg = string.Join(",", response.Errors.ToArray());
+
+            }
+            
+           
+        }
+
+    }
+}

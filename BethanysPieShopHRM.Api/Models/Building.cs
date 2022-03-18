336433c7e509ea489350b0471250750506901f09
Noorudins <noorudins@cloudix.io>
2022-03-18 11:51:29 +0530

    Repository pattern added on API

---
+++ BethanysPieShopHRM.Api/Models/Building.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.Api.Models
+{
+    public class Building
+    {
+        [Key]
+        public int BuildingId { get; set; }
+        public string BuildingName { get; set; }
+        public string BuildingCode { get; set; }
+        public string BuildingIncharge { get; set; }
+        public string Floors { get; set; }
+        public decimal? ERF { get; set; }
+        public decimal? ARF { get; set; }
+        public decimal? WRF { get; set; }
+        public string Remarks { get; set; }
+        public string created_ByUserId { get; set; }
+        public string updated_ByUserId { get; set; }
+        public DateTime? TimeStamp { get; set; }
+    }
+
+    public class BuildingResponse
+    {
+        public string Message { get; set; }
+        public bool IsUpdated { get; set; }
+    }
+}
--- BethanysPieShopHRM.Api/Startup.cs
+++ BethanysPieShopHRM.Api/Startup.cs
@@ -1,5 +1,7 @@
 using BethanysPieShopHRM.Api.Data;
+using BethanysPieShopHRM.Api.Implementation;
 using BethanysPieShopHRM.Api.Models;
+using BethanysPieShopHRM.Api.Repository;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 namespace BethanysPieShopHRM.Api
@@ -36,7 +39,11 @@ namespace BethanysPieShopHRM.Api
 
             services.AddIdentity<IdentityUser, IdentityRole>()
            .AddEntityFrameworkStores<ApplicationDbContext>();
-                       
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Blazor backend", Version = "v1" });
+            });
 
             //services.AddIdentity<IdentityUser, IdentityRole>()
             //.AddEntityFrameworkStores<ApplicationDbContext>();
@@ -62,7 +69,8 @@ namespace BethanysPieShopHRM.Api
             services.AddScoped<IJobCategoryRepository, JobCategoryRepository>();
             services.AddScoped<IEmployeeRepository, EmployeeRepository>();
             services.AddScoped<ICompanyReposistory, CompanyReposistory>();
-         
+            services.AddScoped<IDashboard, DashboardRepo>();
+
             //services.AddTransient<IUserStore<IdentityUser>, UserStore<IdentityUser>>();
             //services.AddDbContext<ApplicationDbContext>();
             //services.AddIdentity<IdentityUser, IdentityRole>()
@@ -84,6 +92,8 @@ namespace BethanysPieShopHRM.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Backend API v1"));
             }
 
             app.UseHttpsRedirection();

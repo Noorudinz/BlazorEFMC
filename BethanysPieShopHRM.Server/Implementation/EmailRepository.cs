159b1ebb1c29e8173f2d5ecbaa4d291c67c32b9e
Noorudins <noorudins@cloudix.io>
2022-05-04 21:09:46 +0530

    Email setting Impelmentation completed

---
+++ BethanysPieShopHRM.Server/Implementation/EmailRepository.cs
@@ -0,0 +1,42 @@
+﻿using BethanysPieShopHRM.Server.Repository;
+using BethanysPieShopHRM.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.Server.Implementation
+{
+    public class EmailRepository: IEmailSetting
+    {
+        private readonly HttpClient _httpClient;
+        public EmailRepository(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<Email> GetEmailSettingDetail()
+        {
+            return await JsonSerializer.DeserializeAsync<Email>
+                (await _httpClient.GetStreamAsync($"api/Email/GetEmail"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+
+        public async Task<Email> UpdateEmailSettingDetail(Email email)
+        {
+            var emailJson =
+                new StringContent(JsonSerializer.Serialize(email), Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("api/Email/UpdateEmailSetting", emailJson);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await JsonSerializer.DeserializeAsync<Email>(await response.Content.ReadAsStreamAsync());
+            }
+
+            return null;
+        }
+    }
+}
---
+++ BethanysPieShopHRM.Server/Repository/IEmailSetting.cs
@@ -0,0 +1,14 @@
+﻿using BethanysPieShopHRM.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.Server.Repository
+{
+    public interface IEmailSetting
+    {
+        Task<Email> GetEmailSettingDetail();
+        Task<Email> UpdateEmailSettingDetail(Email email);
+    }
+}
